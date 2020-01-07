4a89049ec8da73e254d9490edfcbf0a14ff22d20
iwpnd <legionaerr@googlemail.com>
2020-01-07 11:59:40 +0100

    topodict now has its own response_model for the topodict/language/ path

--- toponym-api/app/core/models/output.py
+++ toponym-api/app/core/models/output.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel, StrictStr, Schema
 class Outputtoponym(BaseModel):
     word: StrictStr = Schema(..., title="Input word")
     toponyms: dict = Schema(..., title="Grammatical cases for input word")
+
+
+class Outputtopodict(BaseModel):
+    language: StrictStr = Schema(..., title="Input language")
+    topodictionary: dict = Schema(..., title="Topodict recipe for selected language")
