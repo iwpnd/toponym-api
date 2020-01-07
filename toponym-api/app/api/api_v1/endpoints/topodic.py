9b2e949386922add4ead488a991dadafc42168b3
iwpnd <legionaerr@googlemail.com>
2020-01-07 11:56:26 +0100

    removed /languages endpoint and moved it to its own endpoint

--- toponym-api/app/api/api_v1/endpoints/topodic.py
+++ toponym-api/app/api/api_v1/endpoints/topodic.py
@@ -3,19 +3,22 @@ from fastapi.encoders import jsonable_encoder
 from starlette.exceptions import HTTPException
 from pydantic import BaseModel, StrictStr, Schema
 from toponym import toponym, topodict, settings
+from app.core.models.output import Outputtopodict
 
 router = APIRouter()
 
 
-@router.get("/topodict/{language}", tags=["topodict"])
-def topodic(language: StrictStr):
-    td = topodict.Topodict(language=language.lower())
-    td.load()
+@router.get("/topodict/{language}", response_model=Outputtopodict, tags=["topodict"])
+def topodic_language(language: StrictStr):
+    try:
+        td = topodict.Topodict(language=language.lower())
+        td.load()
 
-    if language.lower() not in settings.LANGUAGE_DICT:
-        raise HTTPException(status_code=404, detail="Language not found")
+        topodic = td._dict
 
-    return td._dict
+        return {"language": language, "topodictionary": topodic}
+    except KeyError:
+        raise HTTPException(status_code=404, detail=f"Language: {language} not found.")
 
 
 @router.get("/topodict/{language}/{ending}", tags=["topodict"])
@@ -30,9 +33,3 @@ def topodic_ending(language: StrictStr, ending: StrictStr):
         raise HTTPException(status_code=404, detail="Ending not found")
 
     return {"language": language, ending: td._dict[ending]}
-
-
-@router.get("/languages", tags=["topodict", "supported languages"])
-def topodic_supported_languages():
-
-    return settings.LANGUAGE_DICT
