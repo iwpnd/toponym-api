9a3e6334ed18effe30389be355236176d5a0d8a7
iwpnd <legionaerr@googlemail.com>
2020-01-15 15:01:07 +0100

    try custom openapi/redoc url

--- toponym_api/main.py
+++ toponym_api/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 from toponym_api.api.api_v1.api import router as api_router
 from toponym_api.core.config import ALLOWED_HOSTS, API_V1_STR, PROJECT_NAME
 from mangum import Mangum
 
-app = FastAPI(title=PROJECT_NAME)
+app = FastAPI(title=PROJECT_NAME, docs_url=None, redoc_url=None)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +20,23 @@ app.add_middleware(
 )
 
 app.include_router(api_router, prefix=API_V1_STR)
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url, title=app.title + " - Swagger UI"
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(openapi_url=app.openapi_url, title=app.title + " - ReDoc")
 
 
 @app.get("/ping")
