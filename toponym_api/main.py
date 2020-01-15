0e90f27fb3ab6086aa985c577c186cb17d672d68
iwpnd <legionaerr@googlemail.com>
2020-01-15 12:30:20 +0100

    change openapi_url

--- toponym_api/main.py
+++ toponym_api/main.py
@@ -4,7 +4,7 @@ from toponym_api.api.api_v1.api import router as api_router
 from toponym_api.core.config import ALLOWED_HOSTS, API_V1_STR, PROJECT_NAME
 from mangum import Mangum
 
-app = FastAPI(title=PROJECT_NAME)
+app = FastAPI(openapi_url=f"{API_V1_STR}/openapi.json", title=PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
