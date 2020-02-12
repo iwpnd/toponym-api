6e22f9cfd6c3f9df3b17af5bea1f7174cb1d1028
iwpnd <legionaerr@googlemail.com>
2020-02-12 16:20:49 +0100

    updated /recipes/{language}/{ending}. updated test_language_ending_route_status

--- tests/test_recipes_endpoint.py
+++ tests/test_recipes_endpoint.py
@@ -1,6 +1,8 @@
 import json
 
 import pytest
+from starlette.status import HTTP_200_OK
+from starlette.status import HTTP_404_NOT_FOUND
 from toponym import settings
 
 from toponym_api.core.config import API_V1_STR
@@ -17,7 +19,7 @@ def is_json(myjson):
 @pytest.mark.parametrize("language", [language for language in settings.LANGUAGE_DICT])
 def test_recipes_language_route(test_app, language):
     response = test_app.get(API_V1_STR + f"/recipes/{language}")
-    assert response.status_code == 200
+    assert response.status_code == HTTP_200_OK
 
 
 @pytest.mark.parametrize("language", [language for language in settings.LANGUAGE_DICT])
@@ -32,17 +34,17 @@ def test_recipes_language_route_default_in_json(test_app, language):
     assert "_default" in response.json()["recipes"]
 
 
-def test_topodict_language_route_language_fails(test_app):
+def test_recipes_language_route_language_fails(test_app):
     language = "test"
     response = test_app.get(API_V1_STR + f"/recipes/{language}")
-    assert response.status_code == 404
+    assert response.status_code == HTTP_404_NOT_FOUND
     assert f"Language: {language} not found" in response.json()["detail"]
 
 
-# def test_language_ending_route_status():
-#     for language in available_languages:
-#         response = client.get(API_V1_STR + f"/topodict/{language}/_default")
-#         assert response.status_code == 200
+@pytest.mark.parametrize("language", [language for language in settings.LANGUAGE_DICT])
+def test_language_ending_route_status(test_app, language):
+    response = test_app.get(API_V1_STR + f"/recipes/{language}/_default")
+    assert response.status_code == HTTP_200_OK
 
 
 # def test_language_ending_route_response_keys():
--- toponym_api/api/api_v1/endpoints/recipes.py
+++ toponym_api/api/api_v1/endpoints/recipes.py
@@ -6,6 +6,7 @@ from starlette.status import HTTP_404_NOT_FOUND
 from toponym.recipes import Recipes
 from toponym.utils import LanguageNotFoundError
 
+from toponym_api.core.models.output import OutputRecipe
 from toponym_api.core.models.output import OutputRecipes
 
 router = APIRouter()
@@ -40,57 +41,59 @@ def recipes_language(language: StrictStr):
         )
 
 
-# @router.get(
-#     "/topodict/{language}/{ending}",
-#     response_model=OutputTopodictRecipe,
-#     tags=["topodictionary"],
-# )
-# def topodic_ending(language: StrictStr, ending: StrictStr):
-#     """
-#     Show the recipe for a word-ending.
+@router.get(
+    "/recipes/{language}/{ending}", response_model=OutputRecipe, tags=["recipes"]
+)
+def topodic_ending(language: StrictStr, ending: StrictStr):
+    """
+    Show the recipe for a word-ending.
 
-#     Given an input language and an ending, present the user with
-#     the recipe that will be used to build grammatical cases
-#     for that specific ending.
+    Given an input language and an ending, present the user with
+    the recipe that will be used to build grammatical cases
+    for that specific ending.
 
-#     e.g.
-#         "д": {
-#         "nominative": [
-#             [""],0
-#             ],
-#         "genitive": [
-#             ["да","дя"],1
-#         ],
-#         "dative": [
-#             ["дю","ду"],1
-#         ],
-#         "accusative": [
-#             ["да","дя"],
-#             1
-#         ],
-#         "instrumental": [
-#             ["дем","дом"],1
-#         ],
-#         "prepositional": [
-#             ["де"],1
-#         ]
-#     }
+    e.g.
+        "д": {
+        "nominative": [
+            [""],0
+            ],
+        "genitive": [
+            ["да","дя"],1
+        ],
+        "dative": [
+            ["дю","ду"],1
+        ],
+        "accusative": [
+            ["да","дя"],
+            1
+        ],
+        "instrumental": [
+            ["дем","дом"],1
+        ],
+        "prepositional": [
+            ["де"],1
+        ]
+    }
 
-#     And this path operation will:
-#     * returns a single recipe for the ending specific in the path
+    And this path operation will:
+    * returns a single recipe for the ending specific in the path
 
-#     """
-#     try:
-#         td = topodict.Topodict(language=language.lower())
-#         td.load()
+    """
+    try:
+        recipes = Recipes(language=language.lower())
+        recipes.load()
 
-#         if ending not in td._dict.keys():
-#             raise HTTPException(status_code=404, detail="Ending not found")
+        if ending not in recipes._dict.keys():
+            raise HTTPException(
+                status_code=HTTP_404_NOT_FOUND, detail="Ending not found"
+            )
 
-#         return {"language": language, "ending": ending, "recipe": td._dict[ending]}
+        return {"language": language, "ending": ending, "recipe": recipes._dict[ending]}
 
-#     except KeyError:
-#         raise HTTPException(status_code=404, detail=f"Language: {language} not found.")
+    except LanguageNotFoundError:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail=f"Language: {language} not found."
+        )
 
 
 # @router.post(
