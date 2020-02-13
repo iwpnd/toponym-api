7e6927fe8bf48db225e8564905cb809a043c54c0
iwpnd <legionaerr@googlemail.com>
2020-02-13 08:34:21 +0100

    updated toponym endpoint to inherit toponym 0.4.1 logic

--- toponym_api/api/api_v1/endpoints/topogen.py
+++ toponym_api/api/api_v1/endpoints/topogen.py
@@ -1,24 +1,19 @@
-from fastapi import APIRouter, Body, Depends, Path, Query
-from fastapi.encoders import jsonable_encoder
+from fastapi import APIRouter
 from starlette.exceptions import HTTPException
-from starlette.responses import JSONResponse
-from starlette.status import (
-    HTTP_200_OK,
-    HTTP_204_NO_CONTENT,
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_422_UNPROCESSABLE_ENTITY,
-)
-from pydantic import BaseModel, StrictStr, Schema
-from toponym import toponym, topodict
-from toponym_api.core.models.output import OutputToponym
+from starlette.status import HTTP_404_NOT_FOUND
+from toponym.recipes import Recipes
+from toponym.toponym import get_longest_word_ending
+from toponym.toponym import Toponym
+from toponym.utils import LanguageNotFoundError
+
 from toponym_api.core.models.input import InputWord
+from toponym_api.core.models.output import OutputToponym
 
 
 router = APIRouter()
 
 
-@router.post("/toponym/", response_model=OutputToponym, tags=["toponym"])
+@router.post("/toponym/", response_model=OutputToponym)
 def topogen_language(inputword: InputWord):
     """
     Create toponyms for input word and language.
@@ -29,19 +24,21 @@ def topogen_language(inputword: InputWord):
     * return toponyms
     """
     try:
-        td = topodict.Topodict(language=inputword.language.lower())
-        td.load()
-        tn = toponym.Toponym(inputword.word, td)
-        tn.build()
-        toponyms = tn.topo
+        recipes = Recipes(language=inputword.language.lower())
+        recipes.load()
+        t = Toponym(input_word=inputword.word, recipes=recipes)
+        t.build()
+        toponyms = t.toponyms
 
         return {
             "word": inputword.word,
             "language": inputword.language,
-            "longest_ending": tn._get_longest_word_ending(inputword.word),
+            "longest_ending": get_longest_word_ending(
+                input_word=inputword.word, recipes=recipes
+            ),
             "toponyms": toponyms,
         }
-    except KeyError as e:
+    except LanguageNotFoundError:
         raise HTTPException(
             status_code=HTTP_404_NOT_FOUND,
             detail=f"Language: {inputword.language} not found.",
